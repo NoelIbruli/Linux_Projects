1b1be4923523414f5059b5935afaa39a21b2ae3a
Noel Ibruli <ibrulinoel@gmail.com>
2021-12-15 16:37:59 -0500

    first commit

---
+++ check_cpu.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+THRESHHOLD=90
+
+echo "CPU is: $CPU"
+
+if [ $CPU -gt $THRESHHOLD ] 2> /dev/null ; then
+	echo "CPU usage is high!"
+else
+	echo "CPU usage is normal!"
+fi
