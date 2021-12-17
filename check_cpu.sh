b15cdfa62043c09f08ae3140245f85c7a986163d
Noel Ibruli <ibrulinoel@gmail.com>
2021-12-17 18:35:26 -0500

    RAM Usage

--- check_cpu.sh
+++ check_cpu.sh
@@ -3,10 +3,20 @@
 EMAIL=ibrulinoel@gmail.com
 
 CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
-THRESHHOLD=90
+THRESHHOLD=0
+RAM=$(echo ""; top -bn1 | grep "MiB Mem" | awk '{print $0}')
+CURRENTMIN=$(top -bn1 | grep "MiB Mem" | awk '{print $6}')
+MIN=7000
 
-echo "CPU is: $CPU%"
+echo "CPU is: $CPU%$RAM"
 
 if [ $CPU -gt $THRESHHOLD ] 2> /dev/null ; then
 	echo "CPU is $CPU%" | mail -s "Cpu usage is very high!" $EMAIL
+	echo "CPU is: $CPU%"
 fi
+
+if [ $CURRENTMIN -lt $MIN ] 2> /dev/null ; then
+	echo "$RAM" | mail -s "RAM is running low!" $EMAIL
+	echo "$RAM"
+fi
+# Issues with $CURRENTMIN -gt $MIN (will not work, probably due to the value of CURRENTMIN)
